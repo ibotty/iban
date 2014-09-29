b831fd0492286b988138a6756dcf4efcef8973a2
Tobias Florek <tob@butter.sh>
2014-09-29 11:02:58 +0200

    sanitize BLZ and AccountNr

--- src/Finance/IBAN/Germany.hs
+++ src/Finance/IBAN/Germany.hs
@@ -10,6 +10,7 @@ module Finance.IBAN.Germany
   ) where
 
 import Control.Arrow (second)
+import Data.Char (isDigit)
 import Data.Text (Text)
 import Finance.IBAN.Internal
 import Finance.IBAN.Germany.Core
@@ -20,7 +21,7 @@ import qualified Data.HashMap.Strict as HM
 import qualified Data.Text as T
 
 ibanFromLegacy :: BLZ -> AccountNr -> (IBAN, Maybe BIC)
-ibanFromLegacy blz account = (IBAN (ibanWithChecksum checksum), mBIC)
+ibanFromLegacy blz' account' = (IBAN (ibanWithChecksum checksum), mBIC)
   where
     mBIC = HM.lookup blz blzBICs
     ibanWithChecksum c = T.concat [cc, c, blz, accountStr]
@@ -30,6 +31,9 @@ ibanFromLegacy blz account = (IBAN (ibanWithChecksum checksum), mBIC)
     checksum = T.pack $ case show (98 - mod97_10 ibanCandidate) of
                           [d,d'] -> [ d,  d']
                           [d']   -> ['0', d']
+    filterNumbers = T.filter isDigit
+    blz = filterNumbers blz'
+    account = filterNumbers account'
 
 legacyFromIBAN :: IBAN -> (BLZ, AccountNr)
 legacyFromIBAN = second (T.dropWhile (== '0')) . T.splitAt 8 . T.drop 4 . rawIBAN
