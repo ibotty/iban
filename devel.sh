d81e1303b8daf02b3b002e46f3226e04bb290ed3
Jan Vogt <jan@noreply.vogts.cloud>
2022-02-03 18:56:13 +0100

    Add more development helpers

--- devel.sh
+++ devel.sh
@@ -2,10 +2,12 @@
 
 usage () {
   cat << USAGE
-Usage: $0 TASK
+Usage: $0 <TASK>
 
 TASK is one of
-- ghcid
+- dev: Open a ghcid that watches for changes and runs all tests.
+- doc [PORT]: Open a hoogle development server. Uses port 3000 by default.
+- repl: Launch a ghci session in the context where tests are run.
 USAGE
 
   exit 0
@@ -14,8 +16,14 @@ USAGE
 [ $# == 0 ] && usage
 
 case $1 in
-	ghcid)
+	dev)
 		ghcid -c "cabal repl test:tests" -T "Main.main" --setup ":set args --hide-successes"
+		;;
+	doc)
+		hoogle server --local -p ${2:-3000} -n
+		;;
+	repl)
+		cabal repl test:tests
 		;;
 	*)
 		usage
